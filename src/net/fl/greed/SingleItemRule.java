46aaf75fda57ef1e987fba5539e5314b0321ee6a
Franco Lombardo <f dot lombardo 69 at gmail dot com>
2016-06-16 21:43:00 +0200

    Modified to improve simmetry

--- src/net/fl/greed/SingleItemRule.java
+++ src/net/fl/greed/SingleItemRule.java
@@ -2,18 +2,18 @@ package net.fl.greed;
 
 public class SingleItemRule implements Rule {
 
-  private int number;
+  private int toFind;
   private int score;
   
-  public SingleItemRule(int number, int score) {
-    this.number = number;
+  public SingleItemRule(int toFind, int score) {
+    this.toFind = toFind;
     this.score = score;
   }
   
   public RuleResult apply(RuleResult previousResult) {
-    int occurrencies = previousResult.occurrenciesOf(number);
+    int occurrencies = previousResult.occurrenciesOf(toFind);
     return previousResult
            .addScore(score * occurrencies)
-           .removeOccurrencies(occurrencies, number);
+           .removeOccurrencies(occurrencies, toFind);
   }
 }
